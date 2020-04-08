c5d4f4c1fcf5e6e88c78dfad43952aff4a85b9cf
Cleber <cleber@ecocentauro.com.br>
2020-04-08 09:14:45 -0300

    Ajuste index para export components

--- lib/client/eco-filter-service.d.ts
+++
@@ -1,3 +0,0 @@
-export interface EcoFilterService {
-    [key: string]: any;
-}
--- lib/client/eco-params-body.d.ts
+++
@@ -1,9 +0,0 @@
-export interface EcoParamsBody {
-    dbid: string;
-    timestamp: string;
-    localid: string;
-    localcode: string;
-    empresa?: string;
-    usuario?: string;
-    senha?: string;
-}
