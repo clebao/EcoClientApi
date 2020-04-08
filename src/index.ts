6365ba74b2697dbec18b5e3303d580a5d3c559a8
Cleber <cleber@ecocentauro.com.br>
2020-04-08 09:12:07 -0300

    Ajuste index para export components

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
-export = require('./client/ecoClientApi')
+export * from './client/ecoClientApi'
+export * from './client/eco-params'
+export * from './client/eco-filter-service'
+export * from './client/eco-params-body'
 
